9362c79baa332f17405b0fda6403d6a8bd973cfa
Leandro <leandro.david.dini@gmail.com>
2023-11-07 19:24:31 -0300

    conexion a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       "type":"mysql",
-      "host":"localhost",
+      "host":"bjndfroukomhvmbd577r-mysql.services.clever-cloud.com",
       "port":3306,
-      "username":"root",
-      "password":"motorola",
-      "database":"lista-prueba",
+      "username":"u2v644nssfmewmsa",
+      "password":"nz20jS2ZkopJmid9oBua",
+      "database":"bjndfroukomhvmbd577r",
       "entities":["dist/**/**.entity{.ts,.js}"],
       "synchronize":false
 
