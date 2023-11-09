ca97e87dd8e7f7cc832de3bf62ffad2595459a66
Leandro <leandro.david.dini@gmail.com>
2023-11-09 11:54:03 -0300

    ultima modificacion

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:['http://localhost:3002','http://192.168.0.9:3002'],
+    origin:['http://localhost:3002','http://192.168.0.9:3002','https://woven-arcadia-403822.web.app/'],
     methods:'GET,PUT,POST,DELETE,PATH',
     credentials: true
   });
