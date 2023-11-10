bb91af6d4ec3d2c6d8a496c59bb4d1f268f91234
Leandro <leandro.david.dini@gmail.com>
2023-11-09 21:07:54 -0300

    Cors agregado

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:['http://localhost:3002','http://192.168.0.9:3002','https://woven-arcadia-403822.web.app'],
+    origin:['http://localhost:3002','http://192.168.0.9:3002','https://woven-arcadia-403822.web.app',"https://lista-tarea-77a2d.web.app"],
     methods:'GET,PUT,POST,DELETE,PATH',
     credentials: true
   });
